d193d4f1ad1b5532fc5395809a6ff8fa6e73919a
Sofia <55833383+Sofilaxus@users.noreply.github.com>
2023-12-13 13:15:52 +0100

    added my enum
    just enum for item categories

---
+++ src/iteminformation/CategoryEnum.java
@@ -0,0 +1,16 @@
+package iteminformation;
+
+/**
+ * Enum class that represents the different categories.
+ *
+ * @author 10021
+ * @version 2022.11.20
+ */
+public enum CategoryEnum
+{
+    FLOOR_LAMINATES,
+    WINDOWS,
+    DOORS,
+    LUMBER,
+    UNKNOWN;
+}
