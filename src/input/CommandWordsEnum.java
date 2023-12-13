e8af6c41e7d29c848b8c9639be5be855ea0088a9
Sofia <55833383+Sofilaxus@users.noreply.github.com>
2023-12-13 13:19:12 +0100

    added my enum for commands
    an enum for commands a user can make in the program

---
+++ src/input/CommandWordsEnum.java
@@ -0,0 +1,27 @@
+package input;
+
+/**
+ * Enum class that represents allowed commands.
+ *
+ * @author 10021
+ * @version 2022.11.09
+ */
+public enum CommandWordsEnum
+{
+    PRINT,
+    ADD,
+    MENU,
+    SEARCH,
+    IN,
+    ID,
+    YES,
+    NO,
+    INVENTORY,
+    DELETE,
+    CATEGORY,
+    DESCRIPTION,
+    COUPON,
+    PRICE,
+    UNKNOWN,
+    EXIT;
+}
